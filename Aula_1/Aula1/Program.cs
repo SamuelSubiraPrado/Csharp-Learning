27c0bd48582183e9040be4755e312c73622070db
Samuel Subirá Prado <149213761+SamuelSubiraPrado@users.noreply.github.com>
2025-01-09 11:40:29 -0300

    Add files via upload

---
+++ Aula_1/Aula1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+//IDENTAÇÃO AUTOMATICA CTRL + K + D
+
+namespace Aula1 //opcional (mas é boa prática)
+{
+    class Program //mesmo nome do arquivo
+    {
+        static void Main(string[] args)
+        {
+            //tipos
+            byte n1 = 126;
+            int n2 = 2147483647;
+            long n3 = 2147483648;
+            bool n4 = true;//ou false;
+            char genero = 'F';
+            float n5 = 4.5f; //Float tem que colocar f no final
+            double n6 = 4.5;
+            string nome = "Maria";
+            int idade = 18;
+            double saldo = 500.99;
+            object obj1 = "Alex"; //equivalente ao var
+            object obj2 = 4.5;  //se adapta ao tipo do dado
+
+            //Controlar o número de casas decimais
+            //que um double/float imprimem na tela, 
+            //F2 = 2 casas, F3 = 3 casas, etc
+            Console.WriteLine(n5.ToString("F2"));
+
+            //Para mudar a impressão de "," para "."
+            //chamamos using System.Globalization;
+            //colocamos após a formatação, CultureInfo.InvariantCulture
+            Console.WriteLine(n5.ToString("F3", CultureInfo.InvariantCulture));
+
+            //Placeholders, concatenação e interpolação
+            
+            //Placeholders, colocamos nas chaves, a ordem em que as var vão entrar
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
+
+            //Interpolação, colocamos $ antes e colocar chaves e suas variaveis
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+
+            //Concatenação, "" + var + "" ...
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais");
+        }
+    }
+}
---
+++ Aula_2/Aula_2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Aula_2_tipos_split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conversão implicita
+            float x = 4.5f;
+            double y = x;
+            Console.WriteLine(y);
+
+            //Double tem 8 bytes, float tem 4, logo não dá 
+            //para converter implicitamente, então temos que
+            //fazer casting(que é uma conversão explicita),
+            //com isso você está arriscando por conta
+            //própria fazer a conversão
+            double a = 5.1;
+            float b = (float)a;
+
+            //Casting também, porém haverá perca de informação
+            //o int irá pegar apenas a parte inteira
+            double c = 5.1;
+            int d = (int)a;
+
+            //a divisão por padrão irá fazer uma div inteira
+            //excluindo a parte flutuante
+            int n1 = 5;
+            int n2 = 2;
+            //O resultado será 2, sendo que deveria ser 2.5
+            double resultado = a / b;
+
+            //PARA CORRIGIR ISSO, TEMOS QUE FAZER UM CASTING
+            resultado = (double) a / b;
+
+            //para não ter que fazer isso, indicamos que um deles é double
+            //por ex: 10.0/5 ou simplesmente def uma das var como double
+
+            //SPLIT
+            //declarar uma variavel do tipo string como vetor
+       
+            string[] vet = Console.ReadLine().Split(' '); //Digitar mais de um valor
+            string p1 = vet[0];
+            string p2 = vet[1];
+            string p3 = vet[2];
+            Console.WriteLine($"{p1} e {p2} e {p3}.");
+
+
+        }
+    }
+}
