caa6875d274ba063f9b49bb5f973d50578ff7b3b
Samuel Subirá Prado <149213761+SamuelSubiraPrado@users.noreply.github.com>
2025-01-09 11:51:21 -0300

    Add files via upload

---
+++ Aula_25_expressao_cond_ternaria/Aula_25_expressao_cond_ternaria/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula_25_expressao_cond_ternaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cond Ternária: Decidir um VALOR com base em uma condição
+            //( condição ) ? valor_se_verdadeiro : valor_se_falso
+
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            //Fariamos dessa forma sem a expressão condicional ternaria
+            /* double desconto;
+            if (preco < 20.0)
+            {
+                desconto = preco * 0.1;
+            }
+            else
+            {
+                desconto = preco * 0.05;
+            }
+
+            Console.WriteLine(desconto);*/
+
+            //-------------------------------------------
+            //Já com a expressão ternária, ficaria assim:
+
+            //Decidir um VALOR com base em uma condição
+            //( condição ) ? valor_se_verdadeiro : valor_se_falso
+
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+            Console.WriteLine(desconto);
+        }
+    }
+}
---
+++ Aula_26_funcoes_string/Aula_26_funcoes_string/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Aula_26_funcoes_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string original = "abcde FGHIJ ABC abc DEFG";
+            Console.WriteLine("Original: " + original);
+            //Passar para maiusculo
+            string s1 = original.ToUpper();
+            Console.WriteLine("To Upper: " + s1);
+
+            //Passar para minusculo
+            string s2 = original.ToLower();
+            Console.WriteLine("To Lower: " + s2);
+
+            //Apaga os espaços em branco no final da string
+            string s3 = original.Trim();
+            Console.WriteLine("Trim: " + s3);
+
+            //Função de procura, onde está aquela ocorrencia
+            int n1 = original.IndexOf("bc");
+            Console.WriteLine("IndexOf('bc'): " + n1);
+
+            //Ultima ocorrencia
+            int n2 = original.LastIndexOf("bc");
+            Console.WriteLine("LastIndexOf('bc'): " + n2);
+
+            //Recortar a string apartir de certo ponto
+            //1) Uso, cortar apartir da posição x
+            string s4 = original.Substring(3);
+            Console.WriteLine("Substring(3): " + s4);
+
+            //2) Uso, cortar apartir da posição x, até y caracteres
+            string s5 = original.Substring(3, 5);
+            Console.WriteLine("Substring(3,5): " + s5);
+
+            //Substituir a parte de um string por outra
+            //1) Uso, substituir um caractere, trocar todo 
+            //caractere 'a' pelo 'x'
+            string s6 = original.Replace('a', 'x');
+            Console.WriteLine("Replace ('a', 'x'): " + s6);
+
+            //Substituir a parte de um string por outra
+            //2) Uso, substituir uma string "abc" por "xy"
+            string s7 = original.Replace("abc", "xy");
+            Console.WriteLine("Replace (\"abc\", \"xy\"): " + s7);
+
+            //Testar se uma string é vazia
+            bool b1 = String.IsNullOrEmpty(original);
+            Console.WriteLine("IsNullOrEmpty: " + b1);
+
+            //Testar se uma string é vazia ou é espaço em branco
+            bool b2 = String.IsNullOrWhiteSpace(original);
+            Console.WriteLine("IsNullOrWhiteSpace: " + b2);
+
+            //Split - recortar uma função com base em algo
+
+        }
+    }
+}
